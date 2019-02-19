b3f7f5d145da3d9df46d920a930440e348ba82f9
Markus Glas <markus.glas@gmx.net>
2019-02-19 16:37:27 +0100

    bugfix

--- MLP/ml_perceptron.py
+++ MLP/ml_perceptron.py
@@ -8,39 +8,24 @@ Gets to 98.40% test accuracy after 20 epochs
 from __future__ import print_function
 
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 
 from MLP.preprocessing import Preprocessor
 
 batch_size = 128
-num_classes = 41
+num_classes = 40
 epochs = 5
 
 # the data, split between train and test sets
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 (x_train, y_train), (x_test, y_test) = Preprocessor().extract_features()
 
-#x_train = x_train.reshape(len(x_train), 13)
-#x_test = x_test.reshape(len(x_test), 13)
-#x_train = x_train.reshape(60000, 784)
-#x_test = x_test.reshape(10000, 784)
-
-#x_train = x_train.astype('float32')
-#x_test = x_test.astype('float32')
-#x_train /= 255
-#x_test /= 255
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1])
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1])
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-#print(y_train, 'train labels')
-#print(y_test, 'test labels')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(13,)))
--- MLP/preprocessing.py
+++ MLP/preprocessing.py
@@ -1,71 +1,37 @@
-import numpy as np
 import pandas as pd
 
 
-class Preprocessor():
+class Preprocessor:
     def __init__(self):
         # Setup parameters
-        self.training_examples_limit = 5000  # 'None' for training on all examples
-        self.test_examples_limit = 200  # 'None' for testing on all examples
+        self.training_examples_limit = None  # 'None' for training on all examples
+        self.test_examples_limit = None  # 'None' for testing on all examples
         self.training_data_file = '../data/TIMIT/train.mfcccsv'
-        self.training_labels_file = '../data/TIMIT/train.targphon'
+        self.training_labels_file = '../data/TIMIT/train.targcsv'
         self.test_data_file = '../data/TIMIT/test.mfcccsv'
-        self.test_labels_file = '../data/TIMIT/test.targphon'
+        self.test_labels_file = '../data/TIMIT/test.targcsv'
 
         self.class_labels_to_int = {}
-        #self.logfile = open('gaussian_classifier_results.log', 'a')
 
     def read_training_test_data(self):
         # Read training data
         df_train = pd.read_csv(self.training_data_file, sep=',', nrows=self.training_examples_limit)
         np_train = df_train.values
 
-        with open(self.training_labels_file, 'r') as f:
-            train_labels = []
-            for row in f.readlines():
-                train_labels.append(row)
+        df_train_labels = pd.read_csv(self.training_labels_file, sep=',', nrows=self.training_examples_limit)
+        train_labels = df_train_labels.values
 
         # Read test data
         df_test = pd.read_csv(self.test_data_file, sep=',', nrows=self.test_examples_limit)
         np_test = df_test.values
 
-        with open(self.test_labels_file, 'r') as f:
-            test_labels = []
-            for row in f.readlines():
-                test_labels.append(row)
+        df_test = pd.read_csv(self.test_labels_file, sep=',', nrows=self.test_examples_limit)
+        test_labels = df_test.values
 
         return np_train, train_labels, np_test, test_labels
 
-    def create_class_labels_dict(self, train_labels):
-        count = 0
-        for label in train_labels:
-            if not label in self.class_labels_to_int.keys():
-                self.class_labels_to_int[label] = count
-                count += 1
-
-    def class_to_int(self, class_label):
-        return self.class_labels_to_int[class_label]
-
-    def int_to_class(self, class_to_int_dict, int):
-        for key, value in self.class_labels_to_int.items():
-            if value == int:
-                return key
-
     def extract_features(self):
         np_train, train_labels, np_test, test_labels = self.read_training_test_data()
-        self.create_class_labels_dict(train_labels)
 
-        y_train = np.zeros(shape=(0, len(train_labels)))
-        y_test = np.zeros(shape=(0, len(test_labels)))
-        for label in train_labels:
-            y_train = np.append(y_train, self.class_to_int(label))
-        for label in test_labels:
-            y_test = np.append(y_test, self.class_to_int(label))
+        return (np_train, train_labels), (np_test, test_labels)
 
-        return (np_train, y_train), (np_test, y_test)
-
-
-if __name__ == '__main__':
-    prep = Preprocessor()
-    (x_train, y_train), (x_test, y_test) = prep.extract_features()
-    print(len(x_test))
