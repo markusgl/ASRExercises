54dd6572abc52f3c8bbab559558488b2c6f34384
williamnf <wnfukunaga@gmail.com>
2019-02-20 12:59:36 +0100

    mlp with neighbor features

--- MLP/ml_perceptron.py
+++ MLP/ml_perceptron.py
@@ -3,29 +3,40 @@ from __future__ import print_function
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Flatten
 from keras.optimizers import RMSprop
 
-from MLP.preprocessing import Preprocessor
+from preprocessing import Preprocessor
 
 batch_size = 128
 num_classes = 40
 epochs = 5
+neighbors = True
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = Preprocessor().extract_features()
+if neighbors:
+	(x_train, y_train), (x_test, y_test) = Preprocessor().extract_with_neighbor_features()
+else:
+	(x_train, y_train), (x_test, y_test) = Preprocessor().extract_features()
 
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1])
-x_test = x_test.reshape(x_test.shape[0], x_test.shape[1])
+	x_train = x_train.reshape(x_train.shape[0], x_train.shape[1])
+	x_test = x_test.reshape(x_test.shape[0], x_test.shape[1])
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(13,)))
+if neighbors:
+	model.add(Dense(512, activation='relu', input_shape=(x_train[0].shape[0], x_train[0].shape[1],)))
+else:
+	model.add(Dense(512, activation='relu', input_shape=(13,)))
 model.add(Dropout(0.2))
+
+if neighbors:
+    model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
+
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
